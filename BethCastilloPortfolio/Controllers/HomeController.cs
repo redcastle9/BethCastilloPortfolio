07020f8c58a81fcc77bd1e5fd9ff768f17d1242d
Beth Castillo <bethc@redcastle9.com>
2017-03-24 10:56:58 -0600

    First Commit

---
+++ BethCastilloPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BethCastilloPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Top()
+        {
+            return View();
+        }
+
+        public ActionResult Work()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
