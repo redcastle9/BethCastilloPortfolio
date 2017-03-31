76f9bc9c33ce9553e1dfd4f8e49bb90b7fe13ee9
Beth Castillo <bethc@redcastle9.com>
2017-03-30 18:24:52 -0600

    Updated Contact form.  It posts now from the live site.

--- BethCastilloPortfolio/Controllers/HomeController.cs
+++ BethCastilloPortfolio/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net.Mail;
 using BethCastilloPortfolio.Models;
+using System.Text;
 
 namespace BethCastilloPortfolio.Controllers
 {
@@ -13,37 +14,41 @@ namespace BethCastilloPortfolio.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult Top()
-        {
-            return View();
-        }
-
-        public ActionResult Work()
-        {
-            return View();
-        }
-
-        public ActionResult Portfolio()
-        {
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
+            ViewBag.Title = "Beth Castillo";
             return View();
         }
 
         [HttpPost]
-        public ActionResult Contact(ContactModel c)
+        public ActionResult Index(ContactModel c)
         {
             if (ModelState.IsValid)
             {
-
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.To.Add("bethc@redcastle9.com");
+                    msg.Subject = c.Subject;
+                    msg.IsBodyHtml = false;
+                    sb.Append("Name: " + c.Name);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Comments: " + c.Message);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View(c);
+                }
+                catch (Exception)
+                {
+                    return View("Index");
+                }
             }
-            return View();
+            return View(c);
         }
     }
 }
--- BethCastilloPortfolio/Models/ContactModel.cs
+++ BethCastilloPortfolio/Models/ContactModel.cs
@@ -10,8 +10,11 @@ namespace BethCastilloPortfolio.Models
     {
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Subject is required")]
         public string Subject { get; set; }
+        [Required(ErrorMessage = "Message is required")]
         public string Message { get; set; }
     }
 }
